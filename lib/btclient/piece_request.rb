4dd4977750fd56c86a0fd72e8632c2c8d77fd04c
D M <gs9600@gmail.com>
2013-08-29 15:23:10 -0400

    added to_s and int_to_4bytehex methods to piece request

--- lib/btclient/piece_request.rb
+++ lib/btclient/piece_request.rb
@@ -1,17 +1,44 @@
+require_relative 'constants'
+include BTClient
+
 module BTClient
   class PieceRequest
     # BT requests are always six
     REQ_ID = 6
     # 16kb is the standard piece length
     STD_PIECE_LEN = 16384
+    # Msg length is always 13
+    MSG_LEN = "\0\0\0\x13"
     attr_accessor :piece_length, :req_id, :req_index,
-                  :req_offset, :block_length
-    def initalize(piece_length, req_index, req_offset)
+                  :req_offset
+    
+    def initialize(req_index, req_offset)
       @piece_length = STD_PIECE_LEN 
+      @req_id = REQ_ID
       @req_index = req_index
       @req_offset = req_offset
     end 
 
-    def 
+    def to_s
+      MSG_LEN + as_hex('req_id') + as_hex('req_index') + 
+        as_hex('req_offset') + as_hex('piece_length')
+    end 
+
+    def as_hex(attr_name)
+      if attr_name.eql? 'req_id'
+        req_id.to_s(16).hexify
+      else
+        int_to_4bytehex(self.send attr_name)  
+      end
+    end 
+
+    # Converts an integer to a 4 byte hex
+    def int_to_4bytehex(int)
+      r = ''
+      twos = int.to_s(16).scan(/../)
+      (1..4-twos.size).each { r << NULL }
+      twos.each { |two| r << two.hexify }
+      r
+    end 
   end 
 end
--- lib/btclient/test.rb
+++ lib/btclient/test.rb
@@ -1,4 +1,10 @@
 require_relative 'util'
+require_relative 'piece_request'
+include BTClient
 
 p '16'.convert_base(10, 16)
 p ['79'.hex].pack("C*")
+
+a = PieceRequest.new(0, 0)
+p a.as_hex('req_offset').inspect
+p a.to_s
