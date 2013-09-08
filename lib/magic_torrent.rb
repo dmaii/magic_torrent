f9c7c82ca60a84f6c243e965f90c540274a94c12
D M <gs9600@gmail.com>
2013-09-08 19:33:32 -0400

    renamed

---
+++ lib/magic_torrent.rb
@@ -0,0 +1,37 @@
+require_relative 'btclient/util'
+require_relative 'btclient/tracker_request'
+require_relative 'btclient/handshake'
+require_relative 'btclient/client'
+require_relative 'btclient/constants'
+require_relative 'btclient/pieces'
+require 'bencode'
+require 'digest/sha1'
+require 'httpclient'
+include BTClient
+
+file = File.open('./testdata/python.torrent')
+#any port in the ephemeral range seems to work
+port = '59696'
+uploaded = 0
+btclient = Client.new(file, port)
+#p btclient.info_hash
+p Util::piece_hashes(btclient.info_hash).size
+their_handshake = btclient.perform_handshake
+#indicate interest
+btclient.socket.send("\0\0\0\x01\x02", 0)
+message_length = btclient.socket.readpartial(4).unpack('C*')
+message_type = btclient.socket.readpartial(1).unpack('C*')
+raw_bitfield = btclient.socket.readpartial(message_length[3]).unpack('C*')
+parsed_bitfield = Util::parse_bitfield raw_bitfield
+p parsed_bitfield
+p parsed_bitfield.size
+puts btclient.socket.readpartial(9).inspect
+#request = "\x00\x00\x00\r\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00@\x00"
+#p Request.new(4, 0).download(btclient.socket).size
+
+# Instantiate Pieces
+#request = "\x00\x00\x00\r\x06\x00\x00\e\b\x00\x00\x00\x00\x00\x00@\x00"
+
+#btclient.socket.send(Request.new(440, 0).to_s, 0)
+#p btclient.socket.readpartial(1024*16)
+btclient.download_file 3
---
+++ lib/magic_torrent/util.rb
@@ -0,0 +1,98 @@
+require_relative 'constants'
+include BTClient
+
+module Util
+
+  # Adds a percentage symbol every two characters
+  # in order to make a hash URL friendly
+  def self.percent_encode_hash(hash)
+    hash = hash.upcase.split('')
+    r = []
+    c = 0
+    until hash.empty?
+      c = 0 if c >= 2
+      r << '%' if c.eql? 0
+      r << hash.shift
+      c += 1
+    end
+    r.join('')
+  end 
+
+  def self.percent_to_hash(percent_encoded)
+    start_array = percent_encoded.split('')
+    r = []
+    while not start_array.empty?
+      c = start_array.first
+      if c.eql? '%'
+        start_array.shift
+        r << start_array.shift.downcase
+        r << start_array.shift.downcase
+      else
+        r << start_array.shift.unpack('H*').join
+      end
+      #puts r.to_s
+    end 
+    r
+  end 
+
+  # Gets all the lengths from the files array of
+  # of hashes, and sums up the lengths to get the
+  # total length of a file
+  def self.sum_lengths(info_files)
+    sum = info_files.inject(0) do |sum, hash|
+      sum += hash['length']
+    end 
+    sum
+  end 
+
+  def self.hash_to_url(hash)
+    r = '?'
+    hash.each_with_index do |(key, value), index|
+      r << '&' if not index.eql? 0
+      r << key.to_s << '=' << value.to_s
+    end 
+    r
+  end 
+
+  def self.parse_bitfield(hexes)
+    bins = hexes.collect { |hex| hex.to_s(2) }
+    bins.join.split('')
+  end 
+
+  # Gets the hashes of the pieces from the info_hash
+  def self.piece_hashes(info_hash)
+    pieces = info_hash['pieces'].scan(/.{20}/)
+    pieces.collect { |hash| hash.unpack('H*').join }
+  end 
+
+end 
+
+class String
+  def convert_base(from, to)
+    self.to_i(from).to_s(to)
+  end
+
+  def remove_hex_escape
+    self.unpack('H*').first
+  end 
+
+  # Turns a hex string into \x{hex} format
+  def hexify
+    [self.hex].pack('C*')
+  end 
+
+end
+
+class Fixnum
+  def to_4_byte_hex
+    r = ''
+    if self.to_s(16).size > 1
+      twos = self.to_s(16).rpartition(/../).reject { |i| i.empty? }
+    else 
+      twos = [self.to_s(16)]
+    end
+    (1..4-twos.size).each { r << NULL }
+    twos.each { |two| r << two.hexify }
+    r
+  end 
+end 
